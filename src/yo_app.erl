d28c1ae4031b7beb87afac36514afa0a5234b3f7
Nick McDowall <nicholasinengland@hotmail.com>
2013-12-03 21:03:36 +0000

    Adding rebar skeleton

---
+++ src/yo_app.erl
@@ -0,0 +1,16 @@
+-module(yo_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    yo_sup:start_link().
+
+stop(_State) ->
+    ok.
