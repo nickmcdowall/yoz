40ef19d9c388bdbb44b2a9f15381108bcdc55c8d
Nick McDowall <nicholasinengland@hotmail.com>
2013-12-03 21:57:38 +0000

    Including Cowboy skeleton

--- src/yo_app.erl
+++ src/yo_app.erl
@@ -5,12 +5,37 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-define(C_ACCEPTORS,  100).
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    Routes    = routes(),
+    Dispatch  = cowboy_router:compile(Routes),
+    Port      = port(),
+    TransOpts = [{port, Port}],
+    ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+    {ok, _}   = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
     yo_sup:start_link().
 
 stop(_State) ->
     ok.
+    
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+routes() ->
+    [
+     {'_', [ {"/", yo_handler, []} ] }
+    ].
+
+port() ->
+    case os:getenv("PORT") of
+        false ->
+            {ok, Port} = application:get_env(http_port),
+            Port;
+        Other ->
+            list_to_integer(Other)
+    end.
